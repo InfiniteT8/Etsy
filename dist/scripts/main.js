538a3c618730d7e7f8df26d5801991039db0c61c
Terry Ross <terryross18@yahoo.com>
2014-07-11 09:57:17 -0400

    updated page

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -9,18 +9,18 @@
 
 // })
 // //with an image variable I query for pictures matching the images string
-// var images;
-// etsyitems.results.forEach(function (a) {
-// 	images = '<div class=“images”><img src="' + a.Images[0].url_570xN + '" style="width: 250px; height: 200px;"/></div>';
+var itemgal = '';
 
-// 	$('.imgbox').append('<div class="box">' + images + '</div>');
-
-// })
-
-var item = '';
-	etsyitems.results.forEach (function (a) {
-	item = '<div class="imgbox"><img src="' + a.Images[0].url_170x135 + '" /><h1>' + a.title + '</h1><h2>' + a.user_id + '</h2><a href="#">' + '$' + a.price + ' ' + a.currency_code +'</a>';
-	item += '</div>';
+etsyitems.results.forEach (function (a) {
+	item = '<li><img src="' + a.Images[0].url_170x135 + '"/><p id="p1">' + a.title + '</p><p id="p2">' + a.user_id + '</p><a href="#">' + '$' + a.price + ' ' + a.currency_code +'</a>';
+	item += '</li>';
 	$('.imgbox').append(item);
-
 });
+
+// var item = '';
+// 	etsyitems.results.forEach (function (a) {
+// 	item = '<div class="imgbox"><img src="' + a.Images[0].url_170x135 + '" /><h1>' + a.title + '</h1><h2>' + a.user_id + '</h2><a href="#">' + '$' + a.price + ' ' + a.currency_code +'</a>';
+// 	item += '</div>';
+// 	$('.imgbox').append(item);
+
+// });
