81ef39c31a54cb39804b1f935d544cbe7f7af945
Terry Ross <terryross18@yahoo.com>
2014-08-27 08:26:12 -0400

    code cleaning

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,3 @@
-// var title;
-// etsyitems.results.forEach(function (a) {
-//   //$('.hero-unit').append(a.title);
-
-//  title = '<p>' + a.title + '</p>' + images ;
-
-//  $('.imgbox').append(title);
-
-
-// })
-// //with an image variable I query for pictures matching the images string
 var itemgal = '';
 
 etsyitems.results.forEach (function (a) {
@@ -16,11 +5,3 @@ etsyitems.results.forEach (function (a) {
 	item += '</li>';
 	$('.imgbox').append(item);
 });
-
-// var item = '';
-// 	etsyitems.results.forEach (function (a) {
-// 	item = '<div class="imgbox"><img src="' + a.Images[0].url_170x135 + '" /><h1>' + a.title + '</h1><h2>' + a.user_id + '</h2><a href="#">' + '$' + a.price + ' ' + a.currency_code +'</a>';
-// 	item += '</div>';
-// 	$('.imgbox').append(item);
-
-// });
